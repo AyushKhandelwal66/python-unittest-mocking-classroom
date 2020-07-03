a1f842c3f776f9e4e0fe9b674a63b1c160a83596
Ayush <akhandelwal680@gmail.com>
2020-07-03 19:28:28 +0530

    Done with Testing

--- src/db_helper.py
+++ src/db_helper.py
@@ -1,15 +1,35 @@
+import pandas as pd
+
+import mysql.connector
+
+mydb=mysql.connector.connect(
+    host="localhost",
+    user="root",
+    passwd="ayush",
+    database="employee",
+    )
+mycursor=mydb.cursor()
+
+sql='SELECT Salary FROM employee'
+mycursor.execute(sql)
+res=mycursor.fetchall()
+
+df=pd.read_sql(sql,mydb)
 class DbHelper:
     def get_maximum_salary(self):
         '''
         Implement the logic to find and return maximum salary from employee table
         '''
-        return None
+        a=df.nlargest(1,['Salary'])
+
+        return a
 
     def get_minimum_salary(self):
         '''
         Implement the logic to find and return minimum salary from employee table
         '''
-        return None
+        b=df.nsmallest(1,['Salary'])
+        return b
 
 if __name__ == "__main__":
     db_helper = DbHelper()
